709e2b4367a9c9b90814e8406bcc7f696e0e2a91
Ainur Yarulin <yarulinainur@gmail.com>
2024-08-01 01:26:59 +0700

    all delete

--- api/bot.ts
+++
@@ -1,9 +0,0 @@
-import { Bot, webhookCallback } from "grammy";
-
-const bot = new Bot(process.env.BOT_TOKEN!);
-
-bot.command("start", (ctx) => ctx.reply("Welcome! Up and running."));
-
-bot.on("message", (ctx) => ctx.reply("Got another message!"));
-
-export default webhookCallback(bot, "std/http");
